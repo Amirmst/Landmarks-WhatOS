9296443bee5adb2a8658ad7c098d3486bd501340
Amir Mostafavi <amireza.mostafavi@yahoo.com>
2019-12-29 17:19:11 +0330

    basic landmark detail implementation

---
+++ Landmarks/WatchLandmarks Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  WatchLandmarks Extension
+//
+//  Created by Amir Mostafavi on 12/29/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
